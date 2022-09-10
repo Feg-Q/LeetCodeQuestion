17a0d39eae7da36212642545d402248468288008
Feg <aikg.feg@outlook.com>
2022-09-10 23:05:34 +0800

    2022.9.10 23:05

---
+++ src/com/feg/leetcode/Question875.java
@@ -0,0 +1,42 @@
+package com.feg.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author Feg
+ * @version 1.0
+ * 二分搜索的拓展问题，找出自变量x、关于x的函数f(x)、目标值target。
+ * 本题中自变量是每小时吃的香蕉的个数，函数f是按照这个个数所需要的总时间，目标值是限定时间H。
+ */
+public class Question875 {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        // right赋一个最大值，然后往中间查找
+        int right = 1000000000 + 1;
+        // 优化一下，如果数组的数据很少，则找出数组中的最大值更快
+        if (piles.length < 20) {
+            right = Arrays.stream(piles).max().getAsInt();
+        }
+        while (left < right){
+            int mid = left + (right - left) / 2;
+            if (getAllHours(piles, mid) <= h){
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        // x的定义域是1到数组中的最大值
+        return left;
+    }
+    public int getAllHours(int[] piles,int x){
+        int allHours = 0;
+        for (int i = 0; i < piles.length; i++) {
+            allHours += piles[i] / x;//吃完这一堆香蕉需要的时间，有偏差。
+            // 补足偏差
+            if (piles[i] % x > 0){
+                allHours++;
+            }
+        }
+        return allHours;
+    }
+}
---
+++ src/com/feg/leetcode/question1_100/Question34.java
@@ -0,0 +1,41 @@
+package com.feg.leetcode.question1_100;
+
+/**
+ * @author Feg
+ * @version 1.0
+ */
+public class Question34 {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[]{-1,-1};
+        int left = 0;
+        int right = nums.length;
+        if (right == 0){
+            return result;
+        }
+        while (left < right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target){
+                right = mid;
+                result[0] = right;
+            }else if (nums[mid] < target){
+                left = mid + 1;
+            } else if (nums[mid] > target){
+                right = mid;
+            }
+        }
+        left = 0;
+        right = nums.length;
+        while (left < right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target){
+                left = mid + 1;
+                result[1] = left - 1;
+            }else if (nums[mid] < target){
+                left = mid + 1;
+            } else if (nums[mid] > target){
+                right = mid;
+            }
+        }
+        return result;
+    }
+}
